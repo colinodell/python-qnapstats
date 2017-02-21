17d89525acb0c5818f9c5673a3fd07166d78ca08
Colin O'Dell <colinodell@gmail.com>
2017-02-21 06:45:40 -0500

    Commit test script

---
+++ test.py
@@ -0,0 +1,34 @@
+import getpass
+import qnapstats
+
+host = input("Host (prefix with 'https://' if needed): ")
+port = int(input("Port: "))
+username = input("Username: ")
+password = getpass.getpass("Password: ")
+
+qnap = qnapstats.QNAPStats(host, port, username, password, debugmode=True, verify_ssl=False)
+
+try:
+    qnap.get_system_stats()
+except:
+    pass
+
+try:
+    qnap.get_system_health()
+except:
+    pass
+
+try:
+    qnap.get_smart_disk_health()
+except:
+    pass
+
+try:
+    qnap.get_volumes()
+except:
+    pass
+
+try:
+    qnap.get_bandwidth()
+except:
+    pass
