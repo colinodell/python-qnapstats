034be8631f516d6b9c6e9abca6e291ec545d7b3b
Colin O'Dell <colinodell@gmail.com>
2021-08-11 16:16:22 -0400

    Drop support for Python 3.4; add support for 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -22,10 +22,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Home Automation',
         'Topic :: System :: Monitoring'
     ],
